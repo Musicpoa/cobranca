e0aa327437486cd924d7dd554ea6de84b9f83c54
Sandro M. dos Santos <sandromarista@gmail.com>
2021-05-26 08:18:53 -0300

    Primeiro Commit

---
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -0,0 +1,115 @@
+package com.algaworks.cobranca.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+public class Titulo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank(message = "Descrição é obrigatório.")
+	@Size(max = 60, message = "A descrição não pode ter mais de 60 caracteres.")
+	private String descricao;
+	
+	@NotNull(message = "Data de vencimento é obrigatória.")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date dataVencimento;
+	
+	@NotNull(message = "Valor é obrigatorio.")
+	@NumberFormat(pattern = "#,##0.00")
+	@DecimalMin(value = "0.01", message = "O valor não pode ser menor que R$ 0,01.")
+	@DecimalMax(value = "9999.99", message = "O valor não pode ser maior que R$ 9.999,99. ")
+	private BigDecimal valor;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusTitulo status;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+	
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public StatusTitulo getStatus() {
+		return status;
+	}
+	public void setStatus(StatusTitulo status) {
+		this.status = status;
+	}
+	
+	public Boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);	
+	};
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Titulo other = (Titulo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
