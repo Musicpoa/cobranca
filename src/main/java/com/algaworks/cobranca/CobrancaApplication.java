f1bbc25bbe2e9398832f477b806ca4e867ff77d3
Sandro M. dos Santos <sandromarista@gmail.com>
2021-06-16 08:30:24 -0300

    Alteração context Path

--- src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 
@@ -26,5 +29,16 @@ public class CobrancaApplication {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 	
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+		
+		
+	}
 
 }
